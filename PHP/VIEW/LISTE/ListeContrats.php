a2ac74ba4f2ae26e57f8b34c0667a089e89e08d1
Pitchoune <webmaster@zelda-series.net>
2023-02-08 15:28:03 +0100

    Ajout de la liste des contrats dans le formutilisateur

--- PHP/VIEW/LISTE/ListeContrats.php
+++ PHP/VIEW/LISTE/ListeContrats.php
@@ -1,11 +1,10 @@
 <?php
 
- echo '<main>';
+echo '<main>';
 
- echo '<div class="flex-0-1"></div>';
+echo '<div class="flex-0-1"></div>';
 
- echo '<div><section class="colonne">';
- 
+echo '<div><section class="colonne">';
 
 $objets = ContratsManager::getList(null, null, null, Parametres::getNbEltParPage());
 echo '<div class="noDisplay NbEltParPage">' . Parametres::getNbEltParPage() . '</div>';
@@ -15,7 +14,7 @@ echo '<div class="grid-col-7 gridListe">';
 
 echo '<div class="caseListe titreListe grid-columns-span-7">Liste des Contrats </div>';
 echo '<div class="bigEspace"></div>';
- echo '<div class="grid-columns-span-7"><div class="demi"></div><input id=searchInList  title="entrer le mot à chercher puis cliquer sur le filtre" placeholder="mot à chercher"/><i class="fa-solid fa-filter" title="entrer le mot à chercher puis cliquer sur le filtre"></i><div class="demi"></div></div>';
+echo '<div class="grid-columns-span-7"><div class="demi"></div><input id=searchInList  title="entrer le mot à chercher puis cliquer sur le filtre" placeholder="mot à chercher"/><i class="fa-solid fa-filter" title="entrer le mot à chercher puis cliquer sur le filtre"></i><div class="demi"></div></div>';
 echo '<div class="caseListe grid-columns-span-7">
 <div></div>
 <div class="bigEspace"></div>
@@ -30,24 +29,27 @@ echo '<div class="caseListe labelListe" data-name= "DateFinContrat">DateFinContr
 
 //Remplissage de div vide pour la structure de la grid
 echo '<div class="caseListe"></div>';
-echo '<div class=" caseListe texteClair ">Nombre d\'éléments :</div><div class="mini" id="nbEnregs"></div> ';
-echo '</div><div class="grid-col-7 gridListe grid-contenu"></div>';
+echo '<div class=" caseListe texteClair ">Nombre d\'éléments :</div><div class="mini" id="nbEnregs">'.count($objets).'</div> ';
+echo '</div><div class="grid-col-7 gridListe grid-contenu">';
 
 // Affichage des enregistrements de la base de données
-echo '<template>';
-echo '<div class="donnees ">IdCentre</div>';
-echo '<div class="donnees ">IdUtilisateur</div>';
-echo '<div class="donnees ">DateDebutContrat</div>';
-echo '<div class="donnees ">DateFinContrat</div>';
- echo '<a href="index.php?page=FormContrats&mode=Afficher&id=IdContrat"><i class="fas fa-file-contract"></i></a>';
-                                    
-echo '<a href="index.php?page=FormContrats&mode=Modifier&id=IdContrat"><i class="fas fa-pen"></i></a>';
-                                    
-echo '<a href="index.php?page=FormContrats&mode=Supprimer&id=IdContrat"><i class="fas fa-trash-alt"></i></a>';
- echo '</template>';
+foreach ($objets AS $value)
+{
+	echo '<div class="donnees ">'.$value->getIdCentre().'</div>';
+	echo '<div class="donnees ">'.$value->getIdUtilisateur().'</div>';
+	echo '<div class="donnees ">'.$value->getDateDebutContrat().'</div>';
+	echo '<div class="donnees ">'.$value->getDateFinContrat().'</div>';
+	echo '<a href="index.php?page=FormContrats&mode=Afficher&id='.$value->getIdContrat().'"><i class="fas fa-file-contract"></i></a>';
+										
+	echo '<a href="index.php?page=FormContrats&mode=Modifier&id='.$value->getIdContrat().'"><i class="fas fa-pen"></i></a>';
+										
+	echo '<a href="index.php?page=FormContrats&mode=Supprimer&id='.$value->getIdContrat().'"><i class="fas fa-trash-alt"></i></a>';
+}
+echo '</div>';
+
 //Derniere ligne du tableau (bouton retour)
 echo '<div class="bigEspace"></div>';
-                                 
+
 echo '<div class="caseListe grid-columns-span-7">
 	<div></div>
 	<a href="index.php?page=Accueil"><button><i class="fas fa-sign-out-alt fa-rotate-180"></i></button></a>
