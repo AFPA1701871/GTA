d3e8287f486b3755a0b1186a12b28867f539514b
alexb60 <alexandrebertrand60@yahoo.fr>
2023-02-08 15:34:55 +0100

    Ajout du last insert id dans DAO add, et modifications des actions contrats et utilisateurs

--- PHP/CONTROLLER/ACTION/ActionUtilisateurs.php
+++ PHP/CONTROLLER/ACTION/ActionUtilisateurs.php
@@ -4,16 +4,17 @@ $elm = new Utilisateurs($_POST);
 switch ($_GET['mode']) {
 	case "Ajouter": {
 		$elm = UtilisateursManager::add($elm);
+		header("location:index.php?page=FormUtilisateurs&mode=Modifier&id=" . $elm);
 		break;
 	}
 	case "Modifier": {
 		$elm = UtilisateursManager::update($elm);
+		header("location:index.php?page=ListeUtilisateurs");
 		break;
 	}
 	case "Supprimer": {
 		$elm = UtilisateursManager::delete($elm);
+		header("location:index.php?page=ListeUtilisateurs");
 		break;
 	}
 }
-
-header("location:index.php?page=ListeUtilisateurs");
