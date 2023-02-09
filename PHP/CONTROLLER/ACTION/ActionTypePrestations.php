7a5be7ae01422bbf0e720f5fc2b3385d45f9ddc7
alexb60 <alexandrebertrand60@yahoo.fr>
2023-02-09 14:19:47 +0100

    Action Types Prestations Ok

--- PHP/CONTROLLER/ACTION/ActionTypePrestations.php
+++ PHP/CONTROLLER/ACTION/ActionTypePrestations.php
@@ -1,19 +1,31 @@
 <?php
 $elm = new TypePrestations($_POST);
 
+// Remplacement des null par 0 pour les checkboxs
+if ($elm->getMotifRequis() == null) {
+	$elm->setMotifRequis(0);
+}
+if ($elm->getUoRequis() == null) {
+	$elm->setUoRequis(0);
+}
+if ($elm->getProjetRequis() == null) {
+	$elm->setProjetRequis(0);
+}
+
 switch ($_GET['mode']) {
 	case "Ajouter": {
 		$elm = TypePrestationsManager::add($elm);
+		header("location:index.php?page=FormTypePrestations&mode=Modifier&id=" . $elm);
 		break;
 	}
 	case "Modifier": {
 		$elm = TypePrestationsManager::update($elm);
+		header("location:index.php?page=ListeTypePrestations");
 		break;
 	}
 	case "Supprimer": {
 		$elm = TypePrestationsManager::delete($elm);
+		header("location:index.php?page=ListeTypePrestations");
 		break;
 	}
 }
-
-header("location:index.php?page=ListeTypePrestations");
