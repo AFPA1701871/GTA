68f19d13832894c539f9c5bc6b6c6050e39518cf
Florent <florent.delaliaux@gmail.com>
2023-02-10 11:33:30 +0100

    commit WE

--- JS/pointage.js
+++ JS/pointage.js
@@ -41,3 +41,34 @@ document.querySelector('#btnMasque').addEventListener('click', function (e) {
         celCache.classList.toggle('noDisplay');
     });
 });
+
+window.addEventListener("load", function setGridPointage(mois, annee){
+    var feuilleStyle;
+    var indexStyle=0;
+    listeStyle=document.styleSheets;
+    console.log(listeStyle);
+    // listeStyle.forEach(element => {
+    //     lien=element.href.split('/');
+    //     if(lien[lien.length-1]=="pointage.css"){
+    //         feuilleStyle=document.styleSheets[indexStyle];
+    //         indexStyle++;
+    //     }
+    // });
+    // nbreJour=new Date(mois-1, annee, 0).getDate();
+    // tailleTotal=rootEl.style.getPropertyValue('--width-col-total');
+    // taillePrct=rootEl.style.getPropertyValue('--width-col-prct');
+    // tailleJo=rootEl.style.getPropertyValue('--width-col-jo');
+    // tailleWe=rootEl.style.getPropertyValue('--width-col-we');
+    // theGridTemplateColumnsValue=tailleTotal+" "+taillePrct+" 0.1fr ";
+    // for (let jour = 1; jour <=nbreJour; jour++) {
+    //     jourActu=new Date(annee, mois-1, jour);
+    //     if(jourActu.getDay()==0||jourActu.getDay()==6){
+    //         theGridTemplateColumnsValue+=tailleWe+" ";
+    //     }
+    //     else{
+    //         theGridTemplateColumnsValue+=tailleJo+" ";
+    //     }
+    // }
+    console.log(feuilleStyle);
+    // rootEl.style.setProperty('--grid-template-columns', theGridTemplateColumnsValue);
+});
