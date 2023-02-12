ffe21c779e7d478a145ea488df8204535e80bf03
Florent <florent.delaliaux@gmail.com>
2023-02-12 19:28:46 +0100

    Ajout Infos Salarié et date sur pointage.html

--- JS/pointage.js
+++ JS/pointage.js
@@ -1,14 +1,14 @@
 
 const sectionSideScroll = document.querySelectorAll('.grid-pointage');
-sectionSideScroll.forEach(element =>{
-    element.addEventListener("wheel",function (e) {    
+sectionSideScroll.forEach(element => {
+    element.addEventListener("wheel", function (e) {
         const race = 125;
         if (e.deltaY > 0) {
             sectionSideScroll.forEach(balise => {
                 balise.scrollLeft += race;
             })
         }
-        else{
+        else {
             sectionSideScroll.forEach(balise => {
                 balise.scrollLeft -= race;
             })
@@ -23,29 +23,33 @@ document.querySelector('#btnMasque').addEventListener('click', function (e) {
     var btn = e.target;
     btn.classList.toggle("fa-expand");
     btn.classList.toggle("fa-contract");
-    var mainGrid = document.querySelector(".mainGrid");
-    mainGrid.classList.toggle("grid-col2-extend");
-    mainGrid.classList.toggle("grid-col2-reduct");
-    colspans = mainGrid.querySelectorAll('.fullLine');
-    colspans.forEach(line => {
-        line.classList.toggle('grid-columns-span-5');
-        line.classList.toggle('grid-columns-span-2');
-    })
-    grid = mainGrid.querySelectorAll('.grid-presta');
-    grid.forEach(subGrid=>{
-        subGrid.classList.toggle("grid-5-extend");
-        subGrid.classList.toggle("grid-5-reduct");
-    });    
     colonneCachable = document.querySelectorAll('.colCachable');
     colonneCachable.forEach(celCache => {
         celCache.classList.toggle('noDisplay');
     });
 });
 
-window.addEventListener("load", function setGridPointage(mois, annee){
+listeLignesPresta = document.querySelectorAll("expand-line");
+console.log(listeLignesPresta);
+// Array.from(listeLignesPresta).forEach(LignePresta => {
+//     LignePresta.addEventListener("click", function (e) {
+//         ligne = e.target.parentNode;
+//         console.log(ligne.getElementsByClassName("colCachable"))
+//         listeCol = ligne.querySelectorAll("colCachable");
+//         console.log(ligne);
+//         console.log(ligne.querySelectorAll("colCachable"));
+//         listeCol.forEach(cell => {
+//             cell.classList.toggle("noDisplay");
+//         });
+
+//     });
+// })
+
+
+window.addEventListener("load", function setGridPointage(mois, annee) {
     var feuilleStyle;
-    var indexStyle=0;
-    listeStyle=document.styleSheets;
+    var indexStyle = 0;
+    listeStyle = document.styleSheets;
     console.log(listeStyle);
     // listeStyle.forEach(element => {
     //     lien=element.href.split('/');
