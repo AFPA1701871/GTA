77b6efdd20d72d25bae49912e6ecb1e7788f5e28
Pitchoune <webmaster@zelda-series.net>
2023-02-14 17:11:06 +0100

    Le mot de passe par défaut est maintenant encodé à l'ajout de l'utilisateur

--- PHP/CONTROLLER/ACTION/ActionUtilisateurs.php
+++ PHP/CONTROLLER/ACTION/ActionUtilisateurs.php
@@ -10,7 +10,7 @@ $elm->setMatriculeUtilisateur($matricule);
 
 switch ($_GET['mode']) {
 	case "Ajouter": {
-		$elm->setPasswordUtilisateur(passwordDefault($elm));
+		$elm->setPasswordUtilisateur(crypte(passwordDefault($elm)));
 		$elm = UtilisateursManager::add($elm);
 		header("location:index.php?page=FormUtilisateurs&mode=Modifier&id=" . $elm);
 		break;
