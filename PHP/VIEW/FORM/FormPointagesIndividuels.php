bc5a26f2bdacd5ed02bd9bd655fb78bcc8fbd278
Val-Del <valentinbruneel1@gmail.com>
2023-02-14 14:49:55 +0100

    css

--- PHP/VIEW/FORM/FormPointagesIndividuels.php
+++ PHP/VIEW/FORM/FormPointagesIndividuels.php
@@ -3,6 +3,8 @@
 echo '<main>';
 echo '<div class="cote"></div>';
 echo '<div class="mainGrid grid-col2-reduct">';
+
+echo '        <div class=" espace"></div>';
 echo '    <div class="grid-columns-span-2 center infosUser">';
 echo '        <div>Année: </div>';
 echo '        <div>';
@@ -43,7 +45,6 @@ echo '        <div>XXXXX</div>';
 echo '        <div></div>';
 echo '        <div>UO d\'affectation:</div>';
 echo '        <div>XXX</div>';
-echo '        <div class="grid-columns-span-17 espace"></div>';
 echo '    </div>';
 
 $anneeVisionne = 2023;
@@ -60,16 +61,16 @@ if (sizeof($listeFermetures) != 0) {
 }
 
 echo '    <!--Séparation Prestations/Pointages => Zone Prestations -->';
-echo '    <div class="grid-presta tabCol grid-5-reduct pointHead bgc4 leftStickyRigth cellBottom">';
+echo '    <div class="grid-presta tabCol grid-5-reduct pointHead bgc4  cellBottom noDisplay">';
 echo '      <div id="anneeSelected" class="noDisplay">2023</div>';
 echo '      <div id="moisSelected" class="noDisplay">02</div>';
 echo '    </div>';
 echo '    <!--Séparation Prestations/Pointages => Zone Pointages -->';
-echo '    <div class="grid-pointage tabCol pointHead">';
+echo '    <div class="grid-pointage tabCol pointHead align-end">';
 
 echo '        <!-- Lignes 9 et 10 -->';
 echo '        <div class="cellBottom center grid-lineDouble bgc4">Total</div>';
-echo '        <div class="cellBottom center grid-lineDouble bgc4">%GTA</div>';
+echo '        <div class="cellBottom center grid-lineDouble bgc4 border-left">%GTA</div>';
 echo '        <div class="cellBottom grid-lineDouble bgc4"></div>';
 
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
@@ -93,9 +94,9 @@ echo '                <!--------------------------->';
 echo '                <!-- Absences              -->';
 echo '                <!--------------------------->';
 
-echo '                <div class="center grid-columns-span-2 fullLine grid-lineSimple cellBottom left titreTypePrestation">0 - Congés, Fériés et absences</div>';
+echo '                <div class="center grid-columns-span-2 fullLine  grid-lineSimple cellBottom left titreTypePrestation">0 - Congés, Fériés et absences</div>';
 echo '                <!-- Ligne -->';
-echo '                <div class="center grid-columns-span-2 fullLine grid-lineSimple cellBottom left">Nb jours absences&nbsp;<small id="info">ne pas saisir les absences RTT</small></div>';
+echo '                <div class="rtt center grid-columns-span-2 fullLine  grid-lineSimple cellBottom left">Nb jours absences&nbsp;<small id="info">ne pas saisir les absences RTT</small></div>';
 echo '        <!--------------------------->';
 echo '        <!-- Prestations de type 1 -->';
 echo '        <!--------------------------->';
@@ -108,9 +109,9 @@ echo '                        <select data-line="1-1">';
 echo '                            <option>Prestations de type 1</option>';
 echo '                        </select>';
 echo '                        <div class="favorise vMini cellRight"><i class="fas fa-fav"></i></div>';
-echo '                        <div class="expand-line vMini"><i class="fas fa-open" data-line="1-1"></i></div>';
+echo '                        <div class="expand-line vMini border-left"><i class="fas fa-open" data-line="1-1"></i></div>';
 echo '                    </div>';
-echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Cde Prest.</div>';
+echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight ">Cde Prest.</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">UO de MAD</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Code Motif</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Code Projet</div>';
@@ -132,7 +133,7 @@ echo '                        <select data-line="2-1">';
 echo '                            <option>Prestations de type 2</option>';
 echo '                        </select>';
 echo '                        <div class="favorise vMini cellRight"><i class="fas fa-fav"></i></div>';
-echo '                        <div class="expand-line vMini"><i class="fas fa-open" data-line="2-1"></i></div>';
+echo '                        <div class="expand-line vMini"><i class="fas fa-open border-left" data-line="2-1"></i></div>';
 echo '                    </div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Cde Prest.</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">UO de MAD</div>';
@@ -156,7 +157,7 @@ echo '                        <select data-line="3-1">';
 echo '                            <option>Prestations de type 3</option>';
 echo '                        </select>';
 echo '                        <div class="favorise vMini cellRight"><i class="fas fa-fav"></i></div>';
-echo '                        <div class="expand-line vMini"><i class="fas fa-open" data-line="3-1"></i></div>';
+echo '                        <div class="expand-line vMini"><i class="fas fa-open border-left" data-line="3-1"></i></div>';
 echo '                    </div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Cde Prest.</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">UO de MAD</div>';
@@ -179,7 +180,7 @@ echo '                        <select data-line="4-1">';
 echo '                            <option>Prestations de type 4</option>';
 echo '                        </select>';
 echo '                        <div class="favorise vMini cellRight"><i class="fas fa-fav"></i></div>';
-echo '                        <div class="expand-line vMini"><i class="fas fa-open" data-line="4-1"></i></div>';
+echo '                        <div class="expand-line vMini"><i class="fas fa-open border-left" data-line="4-1"></i></div>';
 echo '                    </div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Cde Prest.</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">UO de MAD</div>';
@@ -202,7 +203,7 @@ echo '                        <select data-line="5-1">';
 echo '                            <option>Prestations de type 5</option>';
 echo '                        </select>';
 echo '                        <div class="favorise vMini cellRight"><i class="fas fa-fav"></i></div>';
-echo '                        <div class="expand-line vMini"><i class="fas fa-open" data-line="5-1"></i></div>';
+echo '                        <div class="expand-line vMini"><i class="fas fa-open border-left" data-line="5-1"></i></div>';
 echo '                    </div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">Cde Prest.</div>';
 echo '                    <div class="center grid-lineSimple colCachable noDisplay cellBottom cellRight">UO de MAD</div>';
@@ -220,8 +221,8 @@ echo '    </div>';
 
 echo '    <div class="grid-pointage tabCol pointMove">';
 echo '        <div class=" grid-lineSimple cellBottom titreTypePrestation">&nbsp;</div>';
-echo '                <div class="cellBottom center grid-lineSimple">0</div>';
-echo '                <div class="cellBottom center grid-lineSimple"></div>';
+echo '                <div class="cellBottom center grid-lineSimple ">0</div>';
+echo '                <div class="cellBottom center grid-lineSimple border-left"></div>';
 echo '                <div class="cellBottom grid-lineSimple"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
@@ -245,7 +246,7 @@ for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
 echo '                <div class=" grid-lineSimple cellBottom titreTypePrestation">&nbsp;</div>';
 
 echo '        <div data-line="1-1" class="cellBottom center grid-lineDouble">0,00</div>';
-echo '        <div data-line="1-1" class="cellBottom center grid-lineDouble">0,0%</div>';
+echo '        <div data-line="1-1" class="cellBottom center grid-lineDouble border-left">0,0%</div>';
 echo '        <div data-line="1-1" class="cellBottom grid-lineDouble"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
@@ -269,7 +270,7 @@ for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
 echo '        <div class=" grid-lineSimple titreTypePrestation cellBottom">&nbsp;</div>';
 
 echo '        <div data-line="2-1" class="cellBottom center grid-lineDouble">0,00</div>';
-echo '        <div data-line="2-1" class="cellBottom center grid-lineDouble">0,0%</div>';
+echo '        <div data-line="2-1" class="cellBottom center grid-lineDouble border-left">0,0%</div>';
 echo '        <div data-line="2-1" class="cellBottom grid-lineDouble"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
@@ -293,7 +294,7 @@ for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
 echo '        <div class=" titreTypePrestation grid-lineSimple cellBottom">&nbsp;</div>';
 
 echo '        <div data-line="3-1" class="cellBottom center grid-lineDouble">0,00</div>';
-echo '        <div data-line="3-1" class="cellBottom center grid-lineDouble">0,0%</div>';
+echo '        <div data-line="3-1" class="cellBottom center grid-lineDouble border-left">0,0%</div>';
 echo '        <div data-line="3-1" class="cellBottom grid-lineDouble"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
@@ -317,7 +318,7 @@ for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
 echo '        <div class=" grid-lineSimple titreTypePrestation cellBottom">&nbsp;</div>';
 
 echo '        <div data-line="4-1" class="cellBottom center grid-lineDouble">0,00</div>';
-echo '        <div data-line="4-1" class="cellBottom center grid-lineDouble">0,0%</div>';
+echo '        <div data-line="4-1" class="cellBottom center grid-lineDouble border-left">0,0%</div>';
 echo '        <div data-line="4-1" class="cellBottom grid-lineDouble"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
@@ -341,7 +342,7 @@ for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
 echo '        <div class=" titreTypePrestation grid-lineSimple cellBottom">&nbsp;</div>';
 
 echo '        <div data-line="5-1" class="cellBottom center grid-lineDouble">0,00</div>';
-echo '        <div data-line="5-1" class="cellBottom center grid-lineDouble">0,0%</div>';
+echo '        <div data-line="5-1" class="cellBottom center grid-lineDouble border-left">0,0%</div>';
 echo '        <div data-line="5-1" class="cellBottom grid-lineDouble"></div>';
 for ($jour = 1; $jour <= $nbrJoursMois; $jour++) {
     $jourString = $anneeVisionne . "-" . str_pad($moisVisionne, 2, "0", STR_PAD_LEFT) . "-" . str_pad($jour, 2, "0", STR_PAD_LEFT);
