5a79a8dc9e36b6b2498a0eb2ec66fa0c0ff34c7e
Val-Del <valentinbruneel1@gmail.com>
2023-02-17 14:23:16 +0100

    btn change pwd

--- PHP/VIEW/FORM/FormConnexion.php
+++ PHP/VIEW/FORM/FormConnexion.php
@@ -5,7 +5,9 @@ global $regex;
 <main>
     <section class="center colonne">
         <form action="index.php?page=ActionConnexion" method="POST">
-            <div class="colSpan2 center"><h1 class="titre"><?php echo texte('Connexion') ?></h1></div>
+            <div class="colSpan2 center">
+                <h1 class="titre"><?php echo texte('Connexion') ?></h1>
+            </div>
             <div class="colSpan2 center bigEspace"></div>
             <label for="matriculeUtilisateur" class="center"><?php echo texte('matriculeUtilisateur') ?> : </label>
             <input type="text" name="matriculeUtilisateur" required>
@@ -23,5 +25,5 @@ global $regex;
             <input class="btnPrincipale" type="submit" value="<?php echo texte('Se connecter') ?>">
         </form>
     </section>
-    
+
 </main>
