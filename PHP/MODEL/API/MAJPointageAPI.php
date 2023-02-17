6f51ef74743b375ca3fb63b48500a5d09aafee91
Pitchoune <webmaster@zelda-series.net>
2023-02-17 11:18:09 +0100

    Ajout de la gestion des logs

--- PHP/MODEL/API/MAJPointageAPI.php
+++ PHP/MODEL/API/MAJPointageAPI.php
@@ -11,6 +11,20 @@ if ($pointage == null) {
     $newId = PointagesManager::add($elm);
     echo json_encode($newId);
 } else {
+    // En cas de mise à jour, si le pointage modifié était déjà validé, on enregistre l'action
+    // On récupère l'état de validation du pointage - la requête précédente semble vide...
+    $pointages = PointagesManager::getList(["validePointage", "datePointage"], ["idPointage" => $elm->getIdPointage()]);
+
+    // On vérifie si c'est validé
+    if ($pointages[0]->getValidePointage() != null) {
+        // On récupère le nom de la personne auquel le pointage a été modifié
+        $user = UtilisateursManager::findById($elm->getIdUtilisateur());
+
+        // On ajout la modification dans les logs
+        $update = new Logs(['dateLog' => $pointages[0]->getDatePointage(), 'actionLog' => 'Le pointage de '.$user->getNomUtilisateur().' a été modifié', 'idUtilisateur' => $elm->getIdUtilisateur()]);
+        LogsManager::add($update);
+    }
+
     // Update
     PointagesManager::update($elm);
 }
