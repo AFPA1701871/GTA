2289f4713e9c6341deb9e8bcb5beaa17886db617
Fabien59K <fabien.greaux@orange.fr>
2023-02-17 11:32:31 +0100

    updTbManager

--- PHP/VIEW/GENERAL/TbManager.php
+++ PHP/VIEW/GENERAL/TbManager.php
@@ -7,22 +7,24 @@ echo '<div class="cote"></div>';
 echo '<section class="section1">';
 $idManager = $_SESSION['utilisateur']->getIdUtilisateur();
 $agents = UtilisateursManager::getList(['idUtilisateur','nomUtilisateur'], ['idRole'=>1, 'idManager'=>$idManager], 'nomUtilisateur');
-$date = new DateTime();
-$date = moisPrecedent($date);
+$periode = new DateTime();
+$periode = moisPrecedent($periode);
 
     // *** partie combobox mois/annee ***
     echo '<div id="divComboDate">';
         // si dans le mois precedent, il y a au moins un pointage non validé, on affiche le pointage du mois precedent
-        $pointagesNonValide = View_PointagesManager::getList(null, ['idManager'=>$idManager, 'periode'=>$date, 'validePointage'=>0], 'nomUtilisateur');
+        $pointagesNonValide = View_PointagesManager::getList(null, ['idManager'=>$idManager, 'periode'=>$periode, 'validePointage'=>0], 'nomUtilisateur');
         if (empty($pointagesNonValide)){
-            $date = new DateTime();
-            echo creerSelectTab($date->format('Y-m'), tabMoisAnnee(), null, 'comboDate', true);
+            $periode = new DateTime();
+            $periode = $periode->format('Y-m');
+            echo creerSelectTab($periode, tabMoisAnnee(), 'comboDate', true);
         }
         // sinon on affiche le mois en cours
-        else echo creerSelectTab($date, tabMoisAnnee(), null, 'comboDate', true);
+        else echo creerSelectTab($periode, tabMoisAnnee(), 'comboDate', true);
     echo '</div>';
 
     // *** partie tableau agents ***
+    $joursOuvres = getNbJoursOuvres($periode);
     echo '<div id="tabAgents">';
         echo '<div class="vCenter gras">Nom de l\'agent</div>';
         echo '<div class="vCenter gras">Rempli à</div>';
@@ -30,8 +32,12 @@ $date = moisPrecedent($date);
         echo '<div class="cote"></div>';
 
         foreach ($agents as $key => $agent) {
-            $bgc = ($key%2 == 0) ? '': 'bgc';
             $idAgent = $agent->getIdUtilisateur();
+            $pointages = View_PointagesManager::getList(['nbHeuresPointage'], ['periode'=>$periode, 'idUtilisateur'=>$idAgent]);
+            foreach ($pointages as $key => $pointage) {
+                var_dump($pointage);
+            }
+            $bgc = ($key%2 == 0) ? '': 'bgc';
             echo '<div class="vCenter '.$bgc.'">'.$agent->getNomUtilisateur().'</div>';
             echo '<div class="vCenter '.$bgc.'"></div>';
             echo '<div class="vCenter '.$bgc.'"></div>';
