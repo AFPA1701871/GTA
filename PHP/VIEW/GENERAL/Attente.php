d0118cd8dd290204353f54c3dc32a0aa325caea2
Val-Del <valentinbruneel1@gmail.com>
2023-02-28 13:27:37 +0100

    404 update

--- PHP/VIEW/GENERAL/Attente.php
+++ PHP/VIEW/GENERAL/Attente.php
@@ -1,2 +1,12 @@
-<?php
-echo '<p>Encore un peu de patience, <a href=?page=Default>ce</a>  sera bientot ouvert</p>';
+<main>
+<div class="cote">
+</div>
+    <div class="err colonne center">
+        <div>
+        <h1>Travaux en cours, encore un peu de patien<a href=?page=Default>ce</a>!</h1>
+        </div>
+        <img src=".\IMG\404.jpg" alt="">
+    </div>
+<div class="cote">
+</div>
+</main>
