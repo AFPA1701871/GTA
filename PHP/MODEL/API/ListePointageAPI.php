f7bb9b87d7d26c38eb1c2ada2a2c31c4b5fa7360
Florent Delaliaux <florent.delaliaux@gmail.com>
2023-03-21 15:42:02 +0100

    Ajout orderBy pour Uos dans selects

--- PHP/MODEL/API/ListePointageAPI.php
+++ PHP/MODEL/API/ListePointageAPI.php
@@ -2,16 +2,20 @@
 // args = ("table=" + table + "&id=" + id + "&attribut=" + attribut + "&select=" + select);
 //var_dump($_POST);
 $id =  ($_POST['id'] == null) ? null : (int)$_POST['id'];
-$condition = $_POST['condition']=='null' ?  null:json_decode($_POST['condition'],true);
-if ($_POST['select']=="true") {
-    echo creerSelect($id, $_POST['table'], json_decode($_POST['colonne']), $_POST['attribut'], $condition,implode(',', json_decode($_POST['colonne'])), null);
+$condition = $_POST['condition'] == 'null' ?  null : json_decode($_POST['condition'], true);
+if ($_POST['select'] == "true") {
+    if ($_POST['table'] == "Uos") {
+        $orderBy = " convert(`numeroUo`, decimal), `libelleUo` ";
+    } else {
+        $orderBy = implode(',', json_decode($_POST['colonne']));
+    }
+    echo creerSelect($id, $_POST['table'], json_decode($_POST['colonne']), $_POST['attribut'], $condition, $orderBy, null);
 } else {
     $manag =  $_POST['table'] . 'Manager';
-    if ($id != null)
-    {
-        $idName = "id" . substr($_POST['table'],0,-1);
-        $condition[ $idName]= $_POST['id'];
+    if ($id != null) {
+        $idName = "id" . substr($_POST['table'], 0, -1);
+        $condition[$idName] = $_POST['id'];
     }
-    
-    echo json_encode($manag::getList(json_decode($_POST['colonne']),$condition , null, null, true, false));
+
+    echo json_encode($manag::getList(json_decode($_POST['colonne']), $condition, null, null, true, false));
 }
