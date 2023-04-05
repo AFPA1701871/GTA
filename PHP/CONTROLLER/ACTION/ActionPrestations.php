8eb56d92ce98df0ad3ac0d01488ae87e8764ccfe
Florent Delaliaux <florent.delaliaux@gmail.com>
2023-04-05 17:18:51 +0200

    Autoriser la modif d'une prestation pour en retirer ou modifier l'association

--- PHP/CONTROLLER/ACTION/ActionPrestations.php
+++ PHP/CONTROLLER/ACTION/ActionPrestations.php
@@ -1,63 +1,79 @@
 <?php
 $elm = new Prestations($_POST);
 
-if (isset($_POST['idProjet']))
-{
+if (isset($_POST['idProjet'])) {
 	$idprojet = $_POST['idProjet'];
 }
 
 switch ($_GET['mode']) {
 	case "Ajouter": {
-		// On désactive le rendu de < et >
-		$libelle = htmlentities($elm->getLibellePrestation());
-		$elm->setLibellePrestation($libelle);
+			// On désactive le rendu de < et >
+			$libelle = htmlentities($elm->getLibellePrestation());
+			$elm->setLibellePrestation($libelle);
 
-		$elm = PrestationsManager::add($elm);
+			$elm = PrestationsManager::add($elm);
 
-		if ($idprojet)
-		{
-			// On crée l'objet entier
-			$projet = new Associations(['idPrestation' => $elm, 'idProjet' => $idprojet]);
+			if ($idprojet) {
+				// On crée l'objet entier
+				$projet = new Associations(['idPrestation' => $elm, 'idProjet' => $idprojet]);
 
-			// On met à jour l'association
-			$projet = AssociationsManager::add($projet);
+				// On met à jour l'association
+				$projet = AssociationsManager::add($projet);
+			}
+
+			break;
 		}
-
-		break;
-	}
 	case "Modifier": {
-		if ($idprojet)
-		{
+			// On doit récupérer l'id association à partir de id prestation
+			$idasso = AssociationsManager::getList(['idAssociation'], ['idPrestation' => $elm->getIdPrestation()]);
+
+			// On initialise la variable d'idAssociation qu'on utilisera (au cas où l'association n'existe pas encore/plus)
+			$idAssoMod=null;
+
+			// Si l'association existe déjà, on récupère son ID
+			if($idasso){
+				$idAssoMod=$idasso[0]->getIdAssociation();
+			}
+
+			// On crée l'objet entier
+			$projet = new Associations(['idAssociation' => $idAssoMod, 'idPrestation' => $elm->getIdPrestation(), 'idProjet' => $idprojet]);
+
+			// Si l'association n'existait pas/plus
+			if(!$idasso){
+				// On la (re)crée
+				$projet = AssociationsManager::add($projet);
+			}
+			// Sinon, si un idProjet est fournit
+			elseif ($idprojet) {
+				// On met à jour l'association
+				$projet = AssociationsManager::update($projet);
+			}
+			// Si l'association existait mais que l'on ne fournit plus d'idProjet
+			else{
+				// On supprime l'association
+				AssociationsManager::delete($projet);
+			}
+
+			// On désactive le rendu de < et >
+			$libelle = htmlentities($elm->getLibellePrestation());
+			$elm->setLibellePrestation($libelle);
+
+			$elm = PrestationsManager::update($elm);
+			break;
+		}
+	case "Supprimer": {
 			// On doit récupérer l'id association à partir de id prestation
 			$idasso = AssociationsManager::getList(['idAssociation'], ['idPrestation' => $elm->getIdPrestation()]);
 
 			// On crée l'objet entier
-			$projet = new Associations(['idAssociation' => $idasso[0]->getIdAssociation(), 'idPrestation' => $elm->getIdPrestation(), 'idProjet' => $idprojet]);
+			$projet = new Associations(['idAssociation' => $idasso[0]->getIdAssociation()]);
 
-			// On met à jour l'association
-			$projet = AssociationsManager::update($projet);
+			// On supprime l'association
+			$projet = AssociationsManager::delete($projet);
+
+			$elm = PrestationsManager::delete($elm);
+			break;
 		}
-
-		// On désactive le rendu de < et >
-		$libelle = htmlentities($elm->getLibellePrestation());
-		$elm->setLibellePrestation($libelle);
-
-		$elm = PrestationsManager::update($elm);
-		break;
-	}
-	case "Supprimer": {
-		// On doit récupérer l'id association à partir de id prestation
-		$idasso = AssociationsManager::getList(['idAssociation'], ['idPrestation' => $elm->getIdPrestation()]);
-
-		// On crée l'objet entier
-		$projet = new Associations(['idAssociation' => $idasso[0]->getIdAssociation()]);
-
-		// On supprime l'association
-		$projet = AssociationsManager::delete($projet);
-
-		$elm = PrestationsManager::delete($elm);
-		break;
-	}
 }
 
 header("location:index.php?page=ListePrestations");
