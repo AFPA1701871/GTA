5a782e2b71885302f56eb6071603565f062ed799
Florent Delaliaux <florent.delaliaux@gmail.com>
2023-04-06 13:55:34 +0200

    Masque entete colonne visu-modif-del des contrats si visu de l'user

--- PHP/VIEW/LISTE/ListeContrats.php
+++ PHP/VIEW/LISTE/ListeContrats.php
@@ -7,40 +7,41 @@ echo '<div><section class="colonne">';
 $objets = ContratsManager::getList(null, ['idUtilisateur' => $_GET['id']], null, Parametres::getNbEltParPage());
 echo '<div class="noDisplay NbEltParPage">' . Parametres::getNbEltParPage() . '</div>';
 echo '<div class="bigEspace"></div>';
-echo '<div class="bigEspace"></div>';//Création du template de la grid
-echo '<div class="grid-col-'.($formtype?'4-nobutton':7).' gridListe">';
+echo '<div class="bigEspace"></div>'; //Création du template de la grid
+echo '<div class="grid-col-' . ($formtype ? '4-nobutton' : 7) . ' gridListe">';
 
-echo '<div class="caseListe titreListe grid-columns-span-'.($formtype?'4-nobutton':7).'">'.texte("Liste des Contrats").'</div>';
-echo '<div class="caseListe grid-columns-span-'.($formtype?'4-nobutton':7).'">
+echo '<div class="caseListe titreListe grid-columns-span-' . ($formtype ? '4-nobutton' : 7) . '">' . texte("Liste des Contrats") . '</div>';
+echo '<div class="caseListe grid-columns-span-' . ($formtype ? '4-nobutton' : 7) . '">
 <div></div>
 <div class="bigEspace"></div>
-<div class="caseListe">'.(!$formtype?'<a class="pyjama"  href="index.php?page=FormContrats&mode=Ajouter&idutilisateur='.$_GET['id'].'"><i class="fas fa-plus"></i></a>':'').'</div>
+<div class="caseListe">' . (!$formtype ? '<a class="pyjama"  href="index.php?page=FormContrats&mode=Ajouter&idutilisateur=' . $_GET['id'] . '"><i class="fas fa-plus"></i></a>' : '') . '</div>
 <div></div>
 </div>';
 
-echo '<div class="caseListe labelListe borderbottom" data-name= "IdCentre">'.texte("IdCentre").'</div>';
-echo '<div class="caseListe labelListe borderbottom" data-name= "IdUtilisateur">'.texte("IdUtilisateur").'</div>';
-echo '<div class="caseListe labelListe borderbottom" data-name= "DateDebutContrat">'.texte("DateDebutContrat").'</div>';
-echo '<div class="caseListe labelListe borderbottom" data-name= "DateFinContrat">'.texte("DateFinContrat").'</div>';
+echo '<div class="caseListe labelListe borderbottom" data-name= "IdCentre">' . texte("IdCentre") . '</div>';
+echo '<div class="caseListe labelListe borderbottom" data-name= "IdUtilisateur">' . texte("IdUtilisateur") . '</div>';
+echo '<div class="caseListe labelListe borderbottom" data-name= "DateDebutContrat">' . texte("DateDebutContrat") . '</div>';
+echo '<div class="caseListe labelListe borderbottom" data-name= "DateFinContrat">' . texte("DateFinContrat") . '</div>';
 
 //Remplissage de div vide pour la structure de la grid
-echo '<div class="caseListe  borderbottom "></div>';
-echo '<div class=" caseListe texteClair borderbottom " ></div><div class="mini borderbottom" id="nbEnregs"></div> ';
-echo '</div><div class="grid-col-'.($formtype?'4-nobutton':7).' gridListe grid-contenu">';
+if (!$formtype) {
+	echo '<div class="caseListe  borderbottom "></div>';
+	echo '<div class=" caseListe texteClair borderbottom " ></div><div class="mini borderbottom" id="nbEnregs"></div> ';
+	echo '</div><div class="grid-col-' . ($formtype ? '4-nobutton' : 7) . ' gridListe grid-contenu">';
+}
 
 // Affichage des enregistrements de la base de données
-foreach ($objets AS $value)
-{
-	echo '<div class="donnees pyjama ">'.CentresManager::findById($value->getIdCentre())->getNomCentre().'</div>';
-	echo '<div class="donnees pyjama ">'.UtilisateursManager::findById($value->getIdUtilisateur())->getNomUtilisateur().'</div>';
-	echo '<div class="donnees pyjama ">'.date('d/m/Y', strtotime($value->getDateDebutContrat())).'</div>';
-	echo '<div class="donnees pyjama ">'.date('d/m/Y', strtotime($value->getDateFinContrat())).'</div>';
-	if (!$formtype){
-		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Afficher&id='.$value->getIdContrat().'&idutilisateur='.$_GET['id'].'"><i class="fas fa-file-contract"></i></a>';
+foreach ($objets as $value) {
+	echo '<div class="donnees pyjama ">' . CentresManager::findById($value->getIdCentre())->getNomCentre() . '</div>';
+	echo '<div class="donnees pyjama ">' . UtilisateursManager::findById($value->getIdUtilisateur())->getNomUtilisateur() . '</div>';
+	echo '<div class="donnees pyjama ">' . date('d/m/Y', strtotime($value->getDateDebutContrat())) . '</div>';
+	echo '<div class="donnees pyjama ">' . date('d/m/Y', strtotime($value->getDateFinContrat())) . '</div>';
+	if (!$formtype) {
+		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Afficher&id=' . $value->getIdContrat() . '&idutilisateur=' . $_GET['id'] . '"><i class="fas fa-file-contract"></i></a>';
 
-		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Modifier&id='.$value->getIdContrat().'&idutilisateur='.$_GET['id'].'"><i class="fas fa-pen"></i></a>';
+		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Modifier&id=' . $value->getIdContrat() . '&idutilisateur=' . $_GET['id'] . '"><i class="fas fa-pen"></i></a>';
 
-		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Supprimer&id='.$value->getIdContrat().'&idutilisateur='.$_GET['id'].'"><i class="fas fa-trash-alt"></i></a>';
+		echo '<a class="pyjama"  href="index.php?page=FormContrats&mode=Supprimer&id=' . $value->getIdContrat() . '&idutilisateur=' . $_GET['id'] . '"><i class="fas fa-trash-alt"></i></a>';
 	}
 }
 echo '</div></div>';
@@ -59,4 +60,4 @@ echo '<div class="bigEspace grid-columns-span-9"></div>';
 echo '<div class="bigEspace grid-columns-span-9 pagination"></div>';
 
 echo '<div class="bigEspace grid-columns-span-9"></div>';
-echo'</div>';
+echo '</div>';
