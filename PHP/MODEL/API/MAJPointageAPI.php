b8ebc293f8db0520ebab4d82af7d30289461df49
Florent Delaliaux <florent.delaliaux@gmail.com>
2023-04-06 10:13:52 +0200

    Modif MAJPointageAPI pour prevenir doublons en cas probleme reseau a l'enregistrement

--- PHP/MODEL/API/MAJPointageAPI.php
+++ PHP/MODEL/API/MAJPointageAPI.php
@@ -5,17 +5,48 @@ if (!isset($_POST['idPointage'])) {
     if ($_POST['nbHeuresPointage'] != "") {
         // Ajout et retour de l'idPointage créé
         $elm = new Pointages($_POST);
-        $newId = PointagesManager::add($elm);
-        // var_dump($elm);
-        echo json_encode($newId);
+        // Check si pointage déjà dans BDD (problème connexion lors ajout précédent)
+        // on crée le tableau des condition pour rechercher l'id
+        $condCont = $_POST;
+        // on en supprime le nbre d'heures pointées
+        unset($condCont['nbHeuresPointage']);
+        // on met les champs vides à null pour traitement par DAO        
+        foreach ($condCont as $key => $value) {
+            if ($condCont[$key] == "") {
+                $condCont[$key] = null;
+            }
+        }
+        // Recherche si pointage existe déjà
+        $idPointageCont = PointagesManager::getList(["idPointage", "nbHeuresPointage"], $condCont, null, null, true, false);
+
+        // Si le pointage existait déjà
+        if (count($idPointageCont) == 1) {
+            // Si le nombre d'heures est différent
+            if ($idPointageCont[0]["nbHeuresPointage"] != $_POST['nbHeuresPointage']) {
+                // On met à jour l'objet pointage
+                $elm->setIdPointage($idPointageCont[0]["idPointage"]);
+                $elm->setNbHeuresPointage($idPointageCont[0]["nbHeuresPointage"]);
+                // On passe les états Validé et Reporté à 0
+                $elm->setValidePointage(0);
+                $elm->setReportePointage(0);
+                // On sauvegarde la modification
+                PointagesManager::update($elm);
+            }
+            // et on renvoit l'idPointage 
+            echo json_encode($idPointageCont[0]["idPointage"]);
+        } else {
+            // Sinon, c'est un nouveau pointage et on l'ajoute en mémoire
+            $newId = PointagesManager::add($elm);
+            echo json_encode($newId);
+        }
     }
 } else { // modification d'un pointage
     $pointage = PointagesManager::findById($_POST['idPointage']);
-    $nouvNbHeurePoint=(float)$_POST['nbHeuresPointage'];
+    $nouvNbHeurePoint = (float)$_POST['nbHeuresPointage'];
     // Si la nouvelle valeur du pointage est 0 et qu'il n'a pas encore été pris en compte pour le report SIRH
     // Check reportePointage pour gestion affichage dans synthèse
     // Peut pas indiquer qu'on a perdu des heures si pas de trace
-    if ($nouvNbHeurePoint == 0 && $pointage->getReportePointage()==0) {
+    if ($nouvNbHeurePoint == 0 && $pointage->getReportePointage() == 0) {
         // On le supprime
         PointagesManager::delete($pointage);
         echo "Delete";
