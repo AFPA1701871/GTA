60deca6cd247e11a340be4c5cc796a201bf75a6e
afpa1701871 <martine.poix@afpa.fr>
2023-04-25 08:58:35 +0200

    encodage

--- PHP/CONTROLLER/ACTION/ActionMailContenu.php
+++ PHP/CONTROLLER/ACTION/ActionMailContenu.php
@@ -6,7 +6,7 @@ const FROM = "pointage@afpadunkerque.fr";
 // Personnes à contacter en fonction du rôle
 const CONTACT = ["Assistantes" => '<a href="mailto:martine.poix@afpa.fr?subject=GTA">Martine</a>', "Autres" => '<a href="mailto:sylvie.hannequin@afpa.fr?subject=GTA">Sylvie</a> ou <a href="mailto:fanny.fardel@afpa.fr?subject=GTA">Fanny</a>'];
 // Partie header du mail
-const HEADER = 'From: ' . FROM . PHP_EOL . 'MIME-Version: 1.0' . PHP_EOL . 'Content-Type: text/html; charset=UTF-8' . PHP_EOL . 'Content-Transfer-Encoding: base64;';
+const HEADER = 'From: ' . FROM . PHP_EOL . 'MIME-Version: 1.0' . PHP_EOL . 'Content-Type: text/html; charset=UTF-8' . PHP_EOL ;
 // Partie correspondant au début du mail, commune à tous les mails
 const DEBUTMAIL = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>TITRE</title></head><body style="font-family: &quot;Segoe UI&quot;, Tahoma, Geneva, Verdana, sans-serif;"><div style="padding-bottom: 1rem;">Bonjour AGENT,</div>';
 // Tableau contenant le message à afficher dans le mail en fonction de l'état du GTA d'un agent
